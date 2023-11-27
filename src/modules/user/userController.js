0a3e83f357bc81ebf263c7a3fd0fe529c64ade43
Muhammad Eid <32822960+MuhammadEiid@users.noreply.github.com>
2023-11-27 11:25:17 +0200

    Update userController.js

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -14,49 +14,41 @@ const changePassword = handler.changePassword(userModel, "Patient");
 const toggleLikeDislikeBlog = catchError(async (req, res, next) => {
   const { id } = req.params;
 
-  if (req.user.likedBlogs.includes(id)) {
-    // If the user has already liked the blog, remove the like
-    await userModel.findByIdAndUpdate(
-      req.user._id,
-      { $pull: { likedBlogs: id } },
-      { new: true }
-    );
+  const updateOperation = req.user.likedBlogs.includes(id)
+    ? { $pull: { likedBlogs: id } }
+    : { $addToSet: { likedBlogs: id } };
 
-    res.status(200).json({ message: "Blog disliked successfully" });
-  } else {
-    // If the user hasn't liked the blog, like it
-    await userModel.findByIdAndUpdate(
-      req.user._id,
-      { $push: { likedBlogs: id } },
-      { new: true }
-    );
+  const updatedUser = await userModel.findByIdAndUpdate(
+    req.user._id,
+    updateOperation,
+    { new: true }
+  );
 
-    res.status(200).json({ message: "Blog liked successfully" });
-  }
+  const message = req.user.likedBlogs.includes(id)
+    ? "Blog disliked successfully"
+    : "Blog liked successfully";
+
+  res.status(200).json({ message });
 });
 
 const toggleSaveUnsaveBlog = catchError(async (req, res, next) => {
   const { id } = req.params;
 
-  if (req.user.savedBlogs.includes(id)) {
-    // If the user has already saved the blog, unsave it
-    await userModel.findByIdAndUpdate(
-      req.user._id,
-      { $pull: { savedBlogs: id } },
-      { new: true }
-    );
+  const updateOperation = req.user.savedBlogs.includes(id)
+    ? { $pull: { savedBlogs: id } }
+    : { $addToSet: { savedBlogs: id } };
 
-    res.status(200).json({ message: "Blog unsaved successfully" });
-  } else {
-    // If the user hasn't saved the blog, save it
-    await userModel.findByIdAndUpdate(
-      req.user._id,
-      { $push: { savedBlogs: id } },
-      { new: true }
-    );
+  const updatedUser = await userModel.findByIdAndUpdate(
+    req.user._id,
+    updateOperation,
+    { new: true }
+  );
 
-    res.status(200).json({ message: "Blog saved successfully" });
-  }
+  const message = req.user.savedBlogs.includes(id)
+    ? "Blog unsaved successfully"
+    : "Blog saved successfully";
+
+  res.status(200).json({ message });
 });
 
 const getSavedBlogs = handler.getUserSpecificItem(userModel, "savedBlogs");
